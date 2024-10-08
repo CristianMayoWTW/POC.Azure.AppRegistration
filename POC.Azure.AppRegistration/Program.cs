148b2cdbe0665189131786093747a31872d105ed
Cristian Mayo <cristian.mayo@towerswatson.com>
2024-10-08 14:52:29 +0800

    Initial Commit
    - Added initia;l solution file and minimal api project
    - Created an initial service file dedicated for ms-graph integration

---
+++ POC.Azure.AppRegistration/Program.cs
@@ -0,0 +1,29 @@
+using POC.Azure.AppRegistration.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<GraphService>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.MapGet("/api/users/{userPrincipalName}", async (string userPrincipalName, GraphService graphService) =>
+{
+	var user = await graphService.GetUserDetailsAsync(userPrincipalName);
+	return Results.Ok(user);
+})
+.WithName("GetUserByUserPrincipalName")
+.WithOpenApi();
+
+app.Run();
---
+++ POC.Azure.AppRegistration/Services/GraphService.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using Microsoft.Identity.Client;
+
+namespace POC.Azure.AppRegistration.Services
+{
+	// Create a GraphService class to encapsulate the Graph SDK logic
+	public class GraphService
+	{
+		private readonly IConfidentialClientApplication _clientApp;
+		private readonly GraphServiceClient _graphClient;
+
+		public GraphService(IConfiguration configuration)
+		{
+			var clientId = configuration["AzureAd:ClientId"];
+			var tenantId = configuration["AzureAd:TenantId"];
+			var clientSecret = configuration["AzureAd:ClientSecret"];
+
+			_clientApp = ConfidentialClientApplicationBuilder.Create(clientId)
+				.WithClientSecret(clientSecret)
+				.WithAuthority(new Uri($"https://login.microsoftonline.com/{tenantId}"))
+				.Build();
+
+			var tokenCredential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+			_graphClient = new GraphServiceClient(tokenCredential);
+		}
+
+		public async Task<User?> GetUserDetailsAsync(string userPrincipalName)
+		{
+			return await _graphClient.Users[userPrincipalName].GetAsync();
+		}
+	}
+}
